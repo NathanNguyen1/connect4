f4554ae6b683ab972c19183010ed74e84f1adf64
NathanNguyen1 <143215362+NathanNguyen1@users.noreply.github.com>
2024-01-31 11:29:16 -0800

    Add files via upload

---
+++ connect4/Constants.java
@@ -0,0 +1,16 @@
+// extended from tictactoe game code
+public class Constants {
+    // Valid board size
+    public static final int BOARD_SIZEX = 7;
+    public static final int BOARD_SIZEY = 6;
+    
+    // Valid board values
+    public static final int X = -1;
+    public static final int O = 1;
+    public static final int BLANK = 0;
+    
+    // Game States
+    public static final int STANDBY = 0;
+    public static final int GET_X_NAME = 1;
+    
+}
